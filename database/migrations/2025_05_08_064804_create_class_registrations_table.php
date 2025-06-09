b996fdc374beff8087949e4bb91b9762b67f2b80
elangdew <dancowwhehehe@gmail.com>
2025-06-09 16:30:47 +0700

    CI9

--- database/migrations/2025_05_08_064804_create_class_registrations_table.php
+++ database/migrations/2025_05_08_064804_create_class_registrations_table.php
@@ -7,16 +7,15 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-      Schema::create('class_registrations', function (Blueprint $table) {
-    $table->id();
-    $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
-    $table->foreignId('class_id')->nullable()->constrained('fitness_classes')->nullOnDelete();
-    $table->foreignId('schedule_id')->nullable()->constrained('class_schedules')->nullOnDelete();
-    $table->timestamp('registered_at')->useCurrent();
-    $table->enum('payment_status', ['unpaid', 'paid', 'failed'])->default('unpaid');
-    $table->enum('status', ['pending', 'active', 'cancelled'])->default('pending');
-});
-
+        Schema::create('class_registrations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('class_id')->nullable()->constrained('fitness_classes')->nullOnDelete();
+            $table->foreignId('schedule_id')->nullable()->constrained('class_schedules')->nullOnDelete();
+            $table->timestamp('registered_at')->useCurrent();
+            $table->enum('payment_status', ['unpaid', 'paid', 'failed'])->default('unpaid');
+            $table->enum('status', ['pending', 'active', 'cancelled'])->default('pending');
+        });
     }
 
     public function down(): void
